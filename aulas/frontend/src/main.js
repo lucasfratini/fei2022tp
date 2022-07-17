734a325573ab22336386153fd5bd0119d59f5d2d
Lucas <matias_anubis@hotmail.com>
2022-07-16 22:32:02 -0300

    esqueleto visual + vue-roter + apartado de carga

--- aulas/frontend/src/main.js
+++ aulas/frontend/src/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import custom_axios from "@/plugins/axios";
-import VueAxios from "vue-axios";
-//import router from './plugins/router'
+//import custom_axios from "@/plugins/axios";
+//import VueAxios from "vue-axios";
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  //router,
+  router,
   render: h => h(App)
 })
-.use(vuetify)
-.use(VueAxios, custom_axios)
+//.use(vuetify)
+//.use(VueAxios, custom_axios)
 .$mount('#app')
